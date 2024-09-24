a30535dfe22a506eee838e8e9a29b601c4a3d1b7
Liquid <iloveexploiting123@gmail.com>
2024-09-25 07:42:33 +0800

    Update Bedwars.HoldableCPS.lua

--- Bedwars.HoldableCPS.lua
+++ Bedwars.HoldableCPS.lua
@@ -2,7 +2,14 @@ local autoClickerEnabled = false
 local autoClickInterval = 0.00000000000000000000000000000000001
 local UserInputService = game:GetService("UserInputService")
 local RunService = game:GetService("RunService")
+_G.ToggleColor = Color3.fromRGB(255,0,0)
+_G.ButtonColor = Color3.fromRGB(0,255,0)
+_G.SliderColor = Color3.fromRGB(0,0,255)
 
+local library = loadstring(game:HttpGet(('https://garfieldscripts.xyz/ui-libs/wallys-ui.lua')))() -- It's obfuscated, I won't let you see my ugly coding skills. =)
+local w = library:CreateWindow("Holdable CPS")
+
+local b = w:CreateFolder("B")
 local function clickLoop()
     while autoClickerEnabled do
         if UserInputService:IsMouseButtonPressed(Enum.UserInputType.MouseButton1) then
@@ -25,3 +32,28 @@ local function disableAutoClicker()
     autoClickerEnabled = false
     RunService:UnbindFromRenderStep("AutoClicker")
 end
+
+b:Label("NoCPSLimit",{
+    TextSize = 25;
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(69,69,69);
+    
+}) 
+
+b:Toggle("AutoClicker",function(bool)
+ if bool == true then
+        enableAutoClicker()
+else
+    disableAutoClicker()
+end
+end)
+
+b:Slider("CPS",{
+    min = 0.00000000000000000000000000000000001;
+    max = 50;
+    precise = true;
+},function(value)
+    autoClickInterval = value
+end)
+
+
